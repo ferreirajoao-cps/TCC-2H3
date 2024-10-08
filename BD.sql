c2732acd078afe502f560d22f49cfa5f4013da74
kauansiqueira-cps <kauan.siqueira2@etec.sp.gov.br>
2024-10-07 22:38:04 -0300

    fé

--- BD.sql
+++ BD.sql
@@ -1,40 +1,21 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 13/08/2024 às 00:25
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
+create database matemaxdb;
+use matemaxdb;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+create table jogador (
+id_J int auto_increment primary key not null,
+nome varchar(50) not null
+);
 
+create table operacao (
+id_op int auto_increment primary key not null,
+materia char(1) not null,
+nome varchar(20) not null
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `pointskinectquiz`
---
-CREATE DATABASE IF NOT EXISTS `if0_37323049_matemax` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `if0_37323049_matemax`;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `jogadores`
---
-
-CREATE TABLE `jogadores` (
-  `nome` varchar(50) NOT NULL,
-  `pontuacao` int(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+create table pontuacao (
+id_J int not null,
+id_op int not null,
+pontuacao int,
+CONSTRAINT FK_id_J FOREIGN KEY (id_J) REFERENCES jogador(id_J),
+CONSTRAINT FK_id_op FOREIGN KEY (id_op) REFERENCES operacao(id_op)
+);
